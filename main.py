5f7da403f2fdb1bf1a09a9943ba56733e74a1204
PlaceHolderHere <holderhereinplace@gmail.com>
2025-04-12 19:46:25 +0800

    added eval_genomes function

--- main.py
+++ main.py
@@ -1,10 +1,37 @@
 import game
+import neat
+import pickle
 
-env = game.Game2048()
 
-while env.alive:
-    env.update()
-    if env.no_legal_moves():
-        print(f'score:{env.score}')
-        env.print_grid()
-        env.alive = False
+def eval_genomes(genomes, config, num_moves):
+    for i, (genome_id, genome) in enumerate(genomes):
+        environment = game.Game2048()
+        neural_net = neat.nn.FeedForwardNetwork.create(genome, config)
+        for e in range(num_moves):
+            output = neural_net.activate(environment.get_inputs())
+            decision = output.index(max(output))  # [Up, Down, Left, Right]
+
+            if decision == 0:
+                environment.move_up()
+            elif decision == 1:
+                environment.move_down()
+            elif decision == 2:
+                environment.move_left()
+            elif decision == 3:
+                environment.move_right()
+
+            environment.update()
+            if environment.no_legal_moves():
+                break
+
+        genome.fitness = environment.score()
+
+
+
+
+# env = game.Game2048()
+# while env.alive:
+#     env.update()
+#     if env.no_legal_moves():
+#         print(f'score:{env.score}')
+#         env.alive = False
